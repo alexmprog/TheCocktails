108a5012426f0aa9194c2f564494f40ce8cad410
Alexandr Golovach <alexmprog@gmail.com>
2024-10-10 18:06:31 +0300

    mark ui state as stable

--- core/ui/src/main/java/com/alexmprog/thecocktails/core/ui/state/UiState.kt
+++ core/ui/src/main/java/com/alexmprog/thecocktails/core/ui/state/UiState.kt
@@ -2,14 +2,20 @@ package com.alexmprog.thecocktails.core.ui.state
 
 import androidx.annotation.StringRes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.res.stringResource
 
+@Stable
 sealed class UiState<out T> {
+    @Stable
     data object Loading : UiState<Nothing>()
+    @Stable
     data class Error<T>(val error: ErrorText) : UiState<T>()
+    @Stable
     data class Success<T>(val data: T) : UiState<T>()
 }
 
+@Stable
 sealed class ErrorText {
     class StringResource(@StringRes val id: Int): ErrorText()
 
